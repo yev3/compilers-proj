31217f20cd18c1cda395520d73eb716523983a4a
Yevgeni Kamenski <yevsterkamenski@gmail.com>
2017-05-08 14:15:36 -0700

    first commit

---
+++ ReflectiveVisitable.cs
@@ -0,0 +1,10 @@
+﻿namespace ASTBuilder
+{
+	/// <summary>
+	/// Reflective visitor pattern -- a node must accept a visitor </summary>
+	public interface ReflectiveVisitable
+	{
+	   void accept(ReflectiveVisitable rv);
+	}
+
+}
